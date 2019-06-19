729a80bfe3a4372173dabbb48fcfbcfe2e2ba129
Lukas Lindqvist <mail@lukaslindqvist.se>
2019-06-19 16:20:32 +0200

    Finaly fixed the issue with the terminal. The xport at the top of the zshrc fixed it.

--- scripts/dual-screen.sh
+++ scripts/dual-screen.sh
@@ -3,7 +3,9 @@ intern=eDP1
 extern=HDMI1
 
 if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
+    xrandr --dpi 210 --output "$extern" --off --output "$intern" --auto
 else
-    xrandr --output "$intern" --primary --mode 2560x1440 --output "$extern" --mode 1920x1080 --right-of "$intern"
+    xrandr --dpi 210 \
+        --output "$intern" --primary --auto \
+        --output "$extern" --mode 1920x1080 --scale 1.3333x1.3333 --right-of "$intern"
 fi
