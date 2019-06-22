3113ceabfc511a993f5286ab4f360c361859aec1
Lukas Lindqvist <mail@lukaslindqvist.se>
2019-06-22 14:04:05 +0200

    Some changes to polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,12 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar on main screen
-polybar -r main &
+connectedOutputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 
-# Launch bar on external if connected
-if xrandr | grep "HDMI1 connected"; then
-  polybar -r external &
-fi
+for displayId in $connectedOutputs; do
+    MONITOR="$displayId" polybar -r main &
+done
 
 echo "Bars launched..."
--- scripts/dual-screen.sh
+++ scripts/dual-screen.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 intern=eDP1
 extern=HDMI1
+docking=DP1-1
+docking2=DP1-2
 
-if xrandr | grep "$extern disconnected"; then
+if xrandr | grep "$docking disconnected"; then
     xrandr --dpi 210 --output "$extern" --off --output "$intern" --auto
 else
-    xrandr --dpi 210 \
-        --output "$intern" --primary --auto \
-        --output "$extern" --mode 1920x1080 --scale 1.3333x1.3333 --right-of "$intern"
+    xrandr --dpi 96 \
+        --output "$intern" --off \
+        --output "$docking" --primary --mode 1920x1200 --scale 1.25x1.25 --pos 1500x450\
+        --output "$docking2" --mode 1920x1200 --scale 1.25x1.25 --rotate left 
 fi
